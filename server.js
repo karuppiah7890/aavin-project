e8b70c1d9af02f2061092879d84c21f1b060c532
Karuppiah N <karuppiah7890@gmail.com>
2017-06-03 11:22:44 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const plivo = require('plivo')
+const PORT = process.env.PORT || 3000
+
+app.use(bodyparser.urlencoded({ extended: false }))
+
+app.use('/direct-dial', (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2))
+
+  const {
+    ForwardTo,
+    To,
+    CLID,
+    From,
+    DialMusic,
+    DisableCall,
+    CallerName,
+    HangupCause
+  } = req.body
+
+  const to = ForwardTo || To,
+    from = CLID || From
+
+  if(HangupCause) {
+    res.send('SIP Route Hangup Callback')
+    return
+  }
+
+  const xmlResponse = plivo.Response()
+  let isSipUser
+
+  if(!to) {
+    console.log('SIP Route cannot identify destination number')
+    xmlResponse.addHangup()
+  } else {
+    isSipUser = (to.substr(0,4) === 'sip:')
+
+    if(isSipUser && (['all', 'sip'].indexOf(DisableCall) !== -1)) {
+      console.log(`SIP Route calling sip user is disabled : ${DisableCall}`)
+      xmlResponse.addHangup({
+        reason: 'busy'
+      })
+    } else if(isSipUser && (['all', 'number'].indexOf(DisableCall) !== -1)) {
+      console.log(`SIP Route calling number is disabled : ${DisableCall}`);
+      xmlResponse.addHangup({
+        reason: 'busy'
+      })
+    } else {
+      console.log(`SIP Route dialing to ${to}`);
+
+      if(!DialMusic) {
+        xmlResponse.addDial({
+          callerId: from,
+          callerName: CallerName
+        })
+      } else {
+        xmlResponse.addDial({
+          callerId: from,
+          callerName: CallerName,
+          dialMusic: DialMusic
+        })
+      }
+
+      isSipUser? xmlResponse.addUser(to) : xmlResponse.addNumber(to)
+    }
+  }
+
+  res.set('Content-Type', 'text/xml');
+  res.send(xmlResponse.toXML());
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`)
+})
